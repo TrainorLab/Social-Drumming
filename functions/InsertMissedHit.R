ab732bec80283ccde4318830408f10fc6fa297f7
Sean McWeeny <mcweenysean@gmail.com>
2023-03-24 11:16:20 -0400

    InsertMissedHit switched to add_row() syntax

--- functions/InsertMissedHit.R
+++ functions/InsertMissedHit.R
@@ -5,8 +5,6 @@ InsertMissedHit <- function(data, n_skipped){
     skips <- which(data$skip_flag==1)
     new_p <- data[skips[1],]$participant
     
-    data_cut1 <- data[1:(skips[1] - 1),]
-    
     new_row1 <- tibble(participant=new_p,
                        onset_diff_1p=data$roll_1p[skips[1] - 1],
                        roll_1p=data$roll_1p[skips[1] - 1],
@@ -16,25 +14,17 @@ InsertMissedHit <- function(data, n_skipped){
       new_row1$onset_diff_1p[1] <- 1
     }
     
-    
-    data_cut1 <- data_cut1 %>% ungroup() %>%
-      add_row(new_row1, .after = nrow(data_cut1)) %>%
+    data <- data %>% ungroup() %>%
+      add_row(new_row1, .before = (skips[1])) %>%
       group_by(participant)
     
-    # data_cut1 <- data_cut1 %>%
-    #   rows_insert(new_row1, by = names(new_row1))
-    
-    data_til_end <- data[skips[1]:nrow(data),]
-    data_full <- rbind(data_cut1, data_til_end)
-    data_full <- recalc_onsets(data_full)
+    data <- recalc_onsets(data)
     
   } else if(n_skipped == 2){
     
     skips <- which(data$double_skip_flag==1)
     new_p <- data[skips[1],]$participant
     
-    data_cut1 <- data[1:(skips[1] - 1),]
-    
     new_row1 <- tibble(participant=new_p,
                        onset_diff_1p=data$roll_1p[skips[1] - 1],
                        roll_1p=data$roll_1p[skips[1] - 1],
@@ -45,27 +35,21 @@ InsertMissedHit <- function(data, n_skipped){
     }
     
     
-    data_cut1 <- data_cut1 %>%
-      rows_insert(new_row1, by = names(new_row1))
+    data <- data %>% ungroup() %>%
+      add_row(new_row1, .before = (skips[1]))
     
-    data_cut1 <- recalc_onsets(data_cut1)
+    data <- recalc_onsets(data)
     
     new_row2 <- tibble(participant = new_p,
-                       onset_diff_1p = data_cut1$roll_1p[skips[1]],
-                       start_s = data_cut1$roll_1p[skips[1]] + data_cut1$start_s[skips[1]],
+                       onset_diff_1p = data$roll_1p[skips[1]],
+                       start_s = data$roll_1p[skips[1]] + data$start_s[skips[1]],
                        imputed = 1)
     
     
-    data_cut1 <- data_cut1 %>% 
-      rows_insert(new_row2, by = names(new_row2))
+    data <- data %>% ungroup() %>% 
+      add_row(new_row2, .before = (skips[1] + 1))
     
-    
-    
-    
-    data_til_end <- data[skips[1]:nrow(data),]
-    data_full <- rbind(data_cut1, data_til_end)
-    
-    data_full <- recalc_onsets(data_full)
+    data <- recalc_onsets(data)
     
   }
 }
