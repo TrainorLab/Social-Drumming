bf6b5a8a138ea3b94533c1cb2487bcbb9c3a1c88
Sean McWeeny <mcweenysean@gmail.com>
2023-03-24 11:28:10 -0400

    consolidated sync and alt remove_double_hits()

--- RemoveDoubleHits.R
+++
@@ -1,4 +0,0 @@
-RemoveDoubleHits <- function(data){
-  data$double_hit_flag <- data$onset_diff_2p < .15 & (data$participant == lag(data$participant, 1))
-   data <- data %>% filter(double_hit_flag == FALSE)  
-}
--- functions/remove_double_hits.R
+++ functions/remove_double_hits.R
@@ -1,8 +1,17 @@
 remove_double_hits <- function(data){
-  data$double_hit_flag <- ifelse(data$onset_diff_1p < .1, 1, 0)
-  idx <- which(data$double_hit_flag == 1)
-  if(length(idx) > 0){
-    data <- data[-idx,]
+  if(dyad < 200){
+    
+    data$double_hit_flag <- data$onset_diff_2p < .15 & (data$participant == lag(data$participant, 1))
+    data <- data %>% filter(double_hit_flag == FALSE)
+    
+  } else if(dyad > 200 & dyad < 300){
+
+    data$double_hit_flag <- ifelse(data$onset_diff_1p < .15, 1, 0)
+    idx <- which(data$double_hit_flag == 1)
+    if(length(idx) > 0){
+      data <- data[-idx,]
+        
+    }
   }
   return(data)
 }
