f2bab8ccb4c68eeadb4c6461963af8ec14c91a67
Sean McWeeny <mcweenysean@gmail.com>
2023-03-27 16:28:33 -0400

    more generate_stats() updates

--- functions/generate_stats.R
+++ functions/generate_stats.R
@@ -3,11 +3,13 @@ generate_stats <- function(data){
   p_idx <- which(data$participant == 1)
   mean_async <- data %>% group_by(hit_number_participant) %>%
     mutate(async = start_s[1] - start_s[2]) %>%
+    mutate(group_hit = (start_s[1]+start_s[2]) / 2) %>%
     filter(all(imputed == 0))
   
   mean_async <- mean_async[-seq(1, nrow(mean_async), 2),]
   hist(mean_async$async)
-  moa <- mean(abs(mean_async$async))
+  #mean pairwise asynchrony
+  mpa <- mean(abs(mean_async$async))/nrow(mean_async)
   
   ccf_list <- ccf(data$start_s[p_idx], data$start_s[-p_idx])
   lag0_corr <- ccf_list[["acf"]][[17]]
@@ -17,7 +19,7 @@ generate_stats <- function(data){
   #will differ between conditions
   cont_bpm <- n_taps/2
   
-  output <- list(moa, lag0_corr, cont_bpm)
+  output <- list(mpa, lag0_corr, cont_bpm)
   names(output) <- c("Onset Asynchrony", "Lag 0 Cross-Correlation", "Continuation Phase BPM")
   return(output)
 }
