57c3ff04fafacc3e75224797b36bf3aac892b10b
Sean McWeeny <mcweenysean@gmail.com>
2023-06-02 13:25:33 -0400

    removed untrended plot

--- functions/gg_detrend.R
+++ functions/gg_detrend.R
@@ -8,23 +8,12 @@ gg_detrend <- function(data){
     synch_phase_end <- 32
   }
   
-
-    require(gridExtra)
-
-    gg1 <- ggplot(data=data %>% filter(start_s >= synch_phase_end), aes(x=start_s, y=onset_diff_1p, group=as.factor(participant))) +
-      geom_line(aes(color=participant))+
-      geom_point(aes(color=participant))+
-      geom_vline(xintercept = synch_phase_end, size=1)+
-      ggtitle(paste0(dyad, " Trial #",  trial, ": Raw"))+
-      theme_bw()
-    
-    gg2 <- ggplot(data=data, aes(x=start_s_detrend, y=onset_diff_1p_detrend, group=participant)) +
+    gg <- ggplot(data=data, aes(x=start_s_detrend, y=onset_diff_1p_detrend, group=participant)) +
       geom_line(aes(color=participant))+
       geom_point(aes(color=participant))+
       geom_vline(xintercept = synch_phase_end, size=1)+
       ggtitle(paste0(dyad, " Trial #",  trial, ": Detrended"))+
       theme_bw()
     
-     grid_plot <- grid.arrange(gg1, gg2, ncol=2)
-
+  return(gg)
 }
