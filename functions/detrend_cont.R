73879bcf29505f240382b3b9fca07434c7e32cad
Sean McWeeny <mcweenysean@gmail.com>
2023-06-02 13:31:16 -0400

    removed $ notation from tidy syntax

--- functions/detrend_cont.R
+++ functions/detrend_cont.R
@@ -8,14 +8,33 @@ detrend_cont <- function(data, poly = 1){
   data_start <- data %>% filter(start_s < cont_start)
   data_cont <- data %>% filter(start_s >= cont_start)
   
-  data_cont <- data_cont %>% 
-    mutate(onset_diff_2p_detrend = astsa::detrend(onset_diff_2p, order = poly) + onset_diff_2p) %>%
-    mutate(start_s_detrend = onset_diff_2p_detrend + lag(start_s)) %>% 
-    group_by(participant) %>%
-    mutate(onset_diff_1p_detrend = start_s_detrend - lag(start_s_detrend))
   
-  #data_cont$start_s_detrend[2] <- data_cont$start_s_detrend[3] - data_cont$onset_diff_2p_detrend[3]
-  #data_cont$start_s_detrend[1] <- data_cont$start_s_detrend[2] - data_cont$onset_diff_2p_detrend[2]
+  if(dyad <200){
+    data_cont <- data_cont %>% 
+      mutate(onset_diff_2p_detrend = astsa::detrend(onset_diff_2p, order = poly) + onset_diff_2p) %>%
+      mutate(start_s_detrend = onset_diff_2p_detrend + lag(start_s)) %>% 
+      group_by(participant) %>%
+      mutate(onset_diff_1p_detrend = start_s_detrend - lag(start_s_detrend))
+    
+    #data_cont$start_s_detrend[2] <- data_cont$start_s_detrend[3] - data_cont$onset_diff_2p_detrend[3]
+    #data_cont$start_s_detrend[1] <- data_cont$start_s_detrend[2] - data_cont$onset_diff_2p_detrend[2]
+  
+    } else if(dyad >200 &  dyad <300){
+    
+      data_cont <- data_cont %>% 
+        group_by(hit_number_participant) %>%
+        mutate(group_hit = (start_s[1]+start_s[2]) / 2)
+      
+      temp <- astsa::detrend(data_cont$group_hit[seq(1,length(data_cont$group_hit),2)]) 
+      data_cont$start_s_detrend <- data_cont$start_s - unname(rep(temp, each = 2))
+      
+      data_cont <- data_cont %>% 
+        mutate(onset_diff_2p_detrend = start_s_detrend - lag(start_s_detrend)) %>%
+        group_by(participant) %>%
+        mutate(onset_diff_1p_detrend = start_s_detrend - lag(start_s_detrend, 1)) 
+      
+  }  
+  
   
   data <- bind_rows(data_start, data_cont) 
 
