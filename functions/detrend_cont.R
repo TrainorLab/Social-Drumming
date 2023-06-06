db6f94e6a4c0fb74f18cb0ec8c1ed1a04f7651c8
Sean McWeeny <mcweenysean@gmail.com>
2023-06-06 12:59:49 -0400

    detrend_cont() now uses group_2p_onset_avg to be parallel to alternating condition

--- functions/detrend_cont.R
+++ functions/detrend_cont.R
@@ -24,22 +24,23 @@ detrend_cont <- function(data, poly = 1){
       
       data_cont <- data_cont %>% 
         group_by(hit_number_participant) %>%
-        mutate(group_hit = (start_s[1]+start_s[2]) / 2)
+        mutate(group_2p_onset_avg = (onset_diff_2p[1]+onset_diff_2p[2]) / 2)
       
       if(nrow(data_cont) %% 2 == 1){
         data_cont <- data_cont[-nrow(data_cont),]
       }
       
-      temp <- astsa::detrend(data_cont$group_hit[!is.na(data_cont$group_hit[seq(1,length(data_cont$group_hit),2)])])
-      temp <- unname(rep(temp, each = 2))
+    temp <- astsa::detrend(data_cont$group_2p_onset_avg[!is.na(data_cont$group_2p_onset_avg[seq(1,length(data_cont$group_2p_onset_avg),2)])])
+    temp <- unname(temp)
+    
+    data_cont$onset_diff_2p_detrend <- data_cont$onset_diff_2p - temp
+    
+    data_cont <- data_cont %>% 
+      ungroup() %>%
+      mutate(start_s_detrend = onset_diff_2p_detrend + lag(start_s)) %>% 
+      group_by(participant) %>%
+      mutate(onset_diff_1p_detrend = start_s_detrend - lag(start_s_detrend))
       
-      data_cont$start_s_detrend <- data_cont$start_s - temp
-      
-      data_cont <- data_cont %>% 
-        ungroup() %>%
-        mutate(onset_diff_2p_detrend = start_s_detrend - lag(start_s_detrend)) %>%
-        group_by(participant) %>%
-        mutate(onset_diff_1p_detrend = start_s_detrend - lag(start_s_detrend, 1)) 
   }  
   
   
